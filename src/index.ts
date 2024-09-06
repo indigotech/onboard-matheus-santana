491b8261d465b21dbc13fcd3d4b5e0194d7ade79
Matheus22032 <matheus.g2003@hotmail.com>
2024-09-06 10:49:00 -0300

    create mutation creatUser

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,59 @@ const typeDefs = `#graphql
   type Query {
     hello: String
   }
+
+  input UserInput{
+    name: String
+    email: String
+    password: String
+    birthDate: String
+  }
+
+  type User{
+    id: Int
+    name: String
+    email: String
+    password : String
+    birthDate: String
+  }
+
+  type Mutation {
+    createUser(data: UserInput) : User
+  }
+  
 `;
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  birthDate: string;
+}
+
+interface UserInput {
+  name: string;
+  email: string;
+  password: string;
+  birthDate: string;
+}
+
+const users: User[] = [];
+let idIncrement: number = 0;
 
 const resolvers = {
   Query: {
     hello: () => "Hello world!",
+  },
+  Mutation: {
+    createUser: (_: unknown, args: { data: UserInput }): User => {
+      const id = ++idIncrement;
+
+      const user = { ...args.data, id: id };
+
+      users.push(user);
+      return user;
+    },
   },
 };
 
