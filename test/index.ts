9dded9339a3816238bd0909b74160dc8e8eed9e4
Matheus22032 <matheus.g2003@hotmail.com>
2024-09-10 09:50:10 -0300

    create setup for tests

---
+++ test/index.ts
@@ -0,0 +1,27 @@
+import { before, describe, it } from "node:test";
+import axios from "axios";
+import { startServer } from "../src/server.js";
+
+const endpoint = "http://localhost:4000/";
+
+const graphqlQuery = {
+  query: `query fetchHelloWord {
+  hello
+}`,
+};
+
+describe("Hello-world-teste", () => {
+  it("Should return Hello World!, its working", () => {
+    console.log("Hello World!, its working");
+  });
+});
+
+describe("Hello-world-graphql", () => {
+  before(async () => {
+    await startServer();
+  });
+  it("Should return Hello world!", async () => {
+    const response = await axios.post(endpoint, graphqlQuery);
+    console.log(response.data.data.hello);
+  });
+});
