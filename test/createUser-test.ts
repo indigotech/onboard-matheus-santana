f0faa377a2b9c768914d3efc296e81262addc61a
Matheus22032 <matheus.g2003@hotmail.com>
2024-09-16 11:48:27 -0300

    add authentication in createUser test case

--- test/createUser-test.ts
+++ test/createUser-test.ts
@@ -4,9 +4,14 @@ import { expect } from "chai";
 import { endpoint } from "./index.js";
 import { prisma } from "../src/prisma.js";
 import { UserInput } from "../src/resolvers.js";
+import jwt, { Secret } from "jsonwebtoken";
+import { User } from "@prisma/client";
 
 describe("CreateUser-mutation-test", () => {
-  const buildUserInput = (email: string, password: string): UserInput => {
+  const buildUserInput = async (
+    email: string,
+    password: string,
+  ): Promise<UserInput> => {
     return {
       birthDate: "07-12-2003",
       email: email,
@@ -43,15 +48,26 @@ describe("CreateUser-mutation-test", () => {
     };
   };
 
+  let userDb: User;
+
   beforeEach(async () => {
-    await prisma.user.create({
-      data: buildUserInput("matheus.12345@taqtile.com.br", "Teste12345"),
+    userDb = await prisma.user.create({
+      data: await buildUserInput("matheus.12345@taqtile.com.br", "Teste12345"),
     });
   });
 
   it("Should create a user in data base and return that info", async () => {
-    const user = buildUserInput("matheus.54321@hotmail.com", "Teste1235571");
-    const response = await axios.post(endpoint, buildCreateUserMutation(user));
+    const user = await buildUserInput(
+      "matheus.54321@hotmail.com",
+      "Teste1235571",
+    );
+    const token = jwt.sign(
+      { userId: userDb.id },
+      process.env.TOKEN_JWT as Secret,
+    );
+    const response = await axios.post(endpoint, buildCreateUserMutation(user), {
+      headers: { Authorization: token },
+    });
     expect(response.data.data.createUser.birthDate).to.be.eq(user.birthDate);
     expect(response.data.data.createUser.email).to.be.eq(user.email);
     expect(response.data.data.createUser.name).to.be.eq(user.name);
@@ -60,9 +76,15 @@ describe("CreateUser-mutation-test", () => {
     ).to.be.eq(true);
   });
 
-  it("Should create a user in data base and return that info", async () => {
-    const user = buildUserInput("teste.12345@gmail.com", "tes");
-    const response = await axios.post(endpoint, buildCreateUserMutation(user));
+  it("Should return a error password invalid", async () => {
+    const user = await buildUserInput("teste.12345@gmail.com", "tes");
+    const token = jwt.sign(
+      { userId: userDb.id },
+      process.env.TOKEN_JWT as Secret,
+    );
+    const response = await axios.post(endpoint, buildCreateUserMutation(user), {
+      headers: { Authorization: token },
+    });
     const errorMessage = buildErrorMessage(
       "Senha invalida",
       400,
@@ -73,13 +95,35 @@ describe("CreateUser-mutation-test", () => {
   });
 
   it("Should return a error email already exist", async () => {
-    const user = buildUserInput("matheus.12345@taqtile.com.br", "12345Test");
+    const user = await buildUserInput(
+      "matheus.12345@taqtile.com.br",
+      "12345Test",
+    );
     const errorMessage = buildErrorMessage(
       "O email já existe",
       400,
       "O email na qual está tentando ser cadastrado já existe na base de dados",
     );
+    const token = jwt.sign(
+      { userId: userDb.id },
+      process.env.TOKEN_JWT as Secret,
+    );
+    const response = await axios.post(endpoint, buildCreateUserMutation(user), {
+      headers: { Authorization: token },
+    });
 
+    expect(response.data.errors[0]).to.be.deep.eq(errorMessage);
+  });
+  it("Should return a error unauthorized", async () => {
+    const user = await buildUserInput(
+      "matheus.12345@taqtile.com.br",
+      "12345Test",
+    );
+    const errorMessage = buildErrorMessage(
+      "Acesso não permitido",
+      401,
+      "Sem autorização permitida",
+    );
     const response = await axios.post(endpoint, buildCreateUserMutation(user));
 
     expect(response.data.errors[0]).to.be.deep.eq(errorMessage);
