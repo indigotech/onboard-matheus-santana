754128d06b16d3153e1a3cb535a55765f2624b18
Matheus22032 <matheus.g2003@hotmail.com>
2024-09-18 09:42:09 -0300

    decreasing round of encrypted password in seedDatabase

--- seed/seed-database.ts
+++ seed/seed-database.ts
@@ -4,7 +4,7 @@ import seedJson from "./user-seed.json";
 
 const seedDatabase = async () => {
   for (let i = 0; i < seedJson.length; i++) {
-    seedJson[i].password = await hash(seedJson[i].password, 10);
+    seedJson[i].password = await hash(seedJson[i].password, 1);
   }
   await prisma.user.createMany({ data: seedJson });
 };
